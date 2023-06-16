acbe7a93a13e01d03a66e55a6d6e4d8119a88bd7
David Farre <dfarre@vunkers.com>
2023-06-16 12:09:28 +0200

    First changes after fork

--- stunnel.sh
+++ stunnel.sh
@@ -4,24 +4,9 @@ export STUNNEL_CONF="/etc/stunnel/stunnel.conf"
 export STUNNEL_DEBUG="${STUNNEL_DEBUG:-7}"
 export STUNNEL_UID="${STUNNEL_UID:-stunnel}"
 export STUNNEL_GID="${STUNNEL_GID:-stunnel}"
-export STUNNEL_CLIENT="${STUNNEL_CLIENT:-no}"
-export STUNNEL_VERIFY="${STUNNEL_VERIFY:-0}"
-#export STUNNEL_SNI="${STUNNEL_SNI:-}"
-export STUNNEL_CAFILE="${STUNNEL_CAFILE:-/etc/ssl/certs/ca-certificates.crt}"
-export STUNNEL_VERIFY_CHAIN="${STUNNEL_VERIFY_CHAIN:-no}"
-export STUNNEL_KEY="${STUNNEL_KEY:-/etc/stunnel/stunnel.key}"
+export STUNNEL_KEY="${STUNNEL_KEY:-/etc/stunnel/stunel.key}"
 export STUNNEL_CRT="${STUNNEL_CRT:-/etc/stunnel/stunnel.pem}"
 export STUNNEL_DELAY="${STUNNEL_DELAY:-no}"
-export STUNNEL_PROTOCOL_CONFIG_LINE=${STUNNEL_PROTOCOL:+protocol = ${STUNNEL_PROTOCOL}}
-export STUNNEL_PSKSECRETS_CONFIG_LINE=${STUNNEL_PSKSECRETS:+PSKsecrets = ${STUNNEL_PSKSECRETS}}
-
-if [[ ! -z "${STUNNEL_PSKSECRETS}" ]]; then
-    if [[ ! -f "${STUNNEL_PSKSECRETS}" ]]; then
-        echo >&2 "PSKsecrets (${STUNNEL_PSKSECRETS}) doesn't exist"
-        exit 1
-    fi
-    export STUNNEL_CIPHERS_CONFIG_LINE="ciphers = PSK"
-fi
 
 if [[ -z "${STUNNEL_SERVICE}" ]] || [[ -z "${STUNNEL_ACCEPT}" ]] || [[ -z "${STUNNEL_CONNECT}" ]]; then
     echo >&2 "one or more STUNNEL_SERVICE* values missing: "
@@ -31,19 +16,6 @@ if [[ -z "${STUNNEL_SERVICE}" ]] || [[ -z "${STUNNEL_ACCEPT}" ]] || [[ -z "${STU
     exit 1
 fi
 
-if [[ ! -f ${STUNNEL_KEY} ]]; then
-    if [[ -f ${STUNNEL_CRT} ]]; then
-        echo >&2 "crt (${STUNNEL_CRT}) missing key (${STUNNEL_KEY})"
-        exit 1
-    fi
-
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${STUNNEL_KEY} -out ${STUNNEL_CRT} \
-        -config /srv/stunnel/openssl.cnf 
-fi
-
-cp -v ${STUNNEL_CAFILE} /usr/local/share/ca-certificates/stunnel-ca.crt
-cp -v ${STUNNEL_CRT} /usr/local/share/ca-certificates/stunnel.crt
-update-ca-certificates
 
 function trim() {
     awk '{$1=$1};1'
